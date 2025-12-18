6f7b15590438e6e1797421da380e70d9741457a1
vincenzrey <144880422+vincenzrey@users.noreply.github.com>
2025-12-18 16:21:17 +0800

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -2,7 +2,6 @@ import os
 import pandas as pd
 import mlflow
 import mlflow.sklearn
-import dagshub
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
@@ -56,4 +55,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
